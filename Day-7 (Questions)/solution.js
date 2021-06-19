e48d37d3b9e0f6481998160977943b055d644699
SAHIL PRASAD <39877110+sahilp007@users.noreply.github.com>
2021-06-19 10:40:18 +0530

    Day 7 added

--- Day-7 (Questions)/solution.js
+++ Day-7 (Questions)/solution.js
@@ -64,11 +64,14 @@ console.log(" Is ", library[2].title, 'read? ', library[2].readingStatus)
 
 // 5. Write a JavaScript program to get the volume of a Cylinder with four decimal places using object classes. Volume of a cylinder : V = πr2h where r is the radius and h is the height of the cylinder.
 
-cy = {
-	pie: Math.PI,
-	rad: 12,
-	hei: 2,
-	w: 1
-}
-let v = ((cy.pie) * (cy.hei) * (cy.rad) * (cy.rad))
-console.log(v.toFixed(4))
+const Volume = (h, r) => {
+	return Math.PI * h * r * r
+};
+let h = prompt("Enter Height of cylinder ")
+let r = prompt("Enter Radius of cylinder ")
+console.log(Volume(h,r).toFixed(4));
+
+// Solution 6
+const library = [{ title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245}]
+library.sort((a, b) => a.libraryID-b.libraryID)
+console.log(library)
